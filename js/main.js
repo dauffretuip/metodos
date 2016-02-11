5bec5aec6982ab5aa067794bd5cd92d351828204
David Auffret <da@clickbit.net>
2016-02-11 11:25:00 -0500

    better code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var input = $('#numero');
+var tipo = '';
 var binario = $('.binario');
 var decimal = $('.decimal');
 var reset = $('.reset');
@@ -15,10 +16,10 @@ var convertMethod = $('.ConvertMethod');
 var convertedNumber = $('.ConvertedNumber');
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     function checkAlfaStatus() {
-        console.info('Ultimo caracter es un número?', (!alfa) ? 'Si!': 'No!');
+        console.info('Ultimo caracter es un número?', (!alfa) ? 'Si!' : 'No!');
 
         //si es positivo, deshabilitamos los botones
         if (alfa) {
@@ -32,10 +33,10 @@ $(document).ready(function() {
     }
 
     function checkBinStatus() {
-        console.warn('El número es un posible binario?', (bin) ? 'Si!': 'No!');
+        console.warn('El número es un posible binario?', (bin) ? 'Si!' : 'No!');
 
         //si es positivo, deshabilitamos los botones
-        if (!bin){
+        if (!bin) {
             messageBox.removeClass().addClass('ui icon message estado blue');
             messageText.text('Este número solo podrá ser convertido a binario.');
             messageIcon.removeClass().addClass('info circle icon');
@@ -44,10 +45,10 @@ $(document).ready(function() {
     }
 
     function checkIntStatus() {
-        console.warn('El número es un entero?', (int) ? 'Si!': 'No!');
+        console.warn('El número es un entero?', (int) ? 'Si!' : 'No!');
 
         //si es positivo, deshabilitamos los botones
-        if (!int){
+        if (!int) {
             messageBox.removeClass().addClass('ui icon message estado purple');
             messageText.text('Gracias de ingresar un número entero.');
             messageIcon.removeClass().addClass('info announcement icon');
@@ -56,7 +57,7 @@ $(document).ready(function() {
     }
 
     function checkIfAllOk() {
-        if (!alfa && bin){
+        if (!alfa && bin) {
             binario.attr('disabled', false);
             decimal.attr('disabled', false);
             messageBox.addClass('hidden');
@@ -69,7 +70,7 @@ $(document).ready(function() {
         var val = input.val();
 
         //si hay algo ingresado, evaluarlo
-        if(val){
+        if (val) {
 
             //habilitar los botones para permitir la codificación
             binario.attr('disabled', false);
@@ -101,7 +102,6 @@ $(document).ready(function() {
     }
 
 
-
     function resetStatus() {
         //reiniciar el formulario
         binario.attr('disabled', false);
@@ -112,46 +112,96 @@ $(document).ready(function() {
 
 
     function convertToBin() {
-        var val = input.val();
+        var res1 = 999;
+        var args = input.val();
+        while (args > 1) {
+            var arg1 = parseInt(args / 2);
+            var arg2 = args % 2;
+            args = arg1;
+            if (res1 == 999) {
+                res1 = arg2.toString();
+            }
+            else {
+                res1 = arg2.toString() + res1.toString();
+            }
+        }
+        if (args == 1 && res1 != 999) {
+            res1 = args.toString() + res1.toString();
+        }
+        else if (args == 0 && res1 == 999) {
+            res1 = 0;
+        }
+        else if (res1 == 999) {
+            res1 = 1;
+        }
+        var ll = res1.length;
+        while (ll % 4 != 0) {
+            res1 = "0" + res1;
+            ll = res1.length;
+        }
 
-        console.info((val >>> 0).toString(2));
+        return normalize(res1);
 
-        convertingNumber.text(val);
-        convertedNumber.text(decbin(val,16));
     }
 
-    function decbin(dec,length){
-        var out = "";
-        while(length--){
-            out += (dec >> length ) & 1;
+    /**
+     * Se evaluan cada digito por separado.
+     * En este punto ya no importa el numero completo en si, sino cada digito con su posición relativa al numero total
+     * de digitos.
+     * La potencia que se aplica a cada digito es la posición del número desde la derecha.
+     */
+    function convertToDec() {
+
+        var digitos = input.val().toString();
+        var cantidad = digitos.length;
+        var potencia = digitos.length - 1;
+        var suma = 0;
+
+        for (var i = 0; i < cantidad; i ++) {
+            suma += +digitos.charAt(i) * (1 << potencia);
+            potencia--;
         }
-        return out;
+
+        return normalize(suma);
+    }
+
+    /**
+     * Escribir el resultado de la operación en la caja verde
+     * @param respuesta es el numero calculado en las funciones anteriores
+     */
+    function normalize(respuesta) {
+        convertingNumber.text(input.val());
+        convertMethod.text(tipo);
+        convertedNumber.text(respuesta);
     }
 
 
     //llamar la funcion que maneja la verificación del numero
-    input.on('keyup', function() {
+    input.on('keyup', function () {
         console.clear();
         typeNumber();
     });
 
     //ejecutar la funcion de reinicio con el boton de reinicio
-    reset.on('click', function() {
+    reset.on('click', function () {
         console.clear();
         resultado.hide();
         resetStatus();
     });
 
     //si le das click al boton de binario, ejecutar esto
-    binario.on('click', function() {
+    binario.on('click', function () {
+        tipo = 'binario';
         convertingNumber.text(input.val());
         resultado.show();
         convertToBin();
     });
 
     //si le das click al boton de decimal, ejecutar esto
-    decimal.on('click', function() {
+    decimal.on('click', function () {
+        tipo = 'decimal';
         resultado.show();
+        convertToDec();
     });
 });
 
